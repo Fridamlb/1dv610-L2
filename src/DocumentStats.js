99cc2c0c55a42c7b16583689042849e0bbaa60a1
Frida Berg <fb223gw@student.lnu.se>
2025-09-29 18:37:29 +0200

    Added Paragraph funktion

--- src/DocumentStats.js
+++ src/DocumentStats.js
@@ -1,11 +1,12 @@
 import WordAnalyzer from './WordAnalyzer.js'
 import SentenceAnalyzer from './SentenceAnalyzer.js'
-
+import ParagraphAnalyzer from './ParagraphAnalyzer.js'
 export default class DocumentStats {
     constructor(text) {
         this.text = text;
         this.wordAnalyzer = new WordAnalyzer(text)
         this.sentenceAnalyzer = new SentenceAnalyzer(text)
+        this.paragraphAnalyzer = new ParagraphAnalyzer(text)
     }
     getWordCount() {
       return this.wordAnalyzer.countWords()
@@ -15,10 +16,15 @@ export default class DocumentStats {
       return this.sentenceAnalyzer.countSentences()
     }
 
+    getParagraphCount() {
+      return this.paragraphAnalyzer.countParagraphs()
+    }
+
      summary() {
         return {
             words: this.getWordCount(),
-            sentences: this.getSentenceCount()
+            sentences: this.getSentenceCount(),
+            paragraphs: this.getParagraphCount(),
         }
     }
   }
