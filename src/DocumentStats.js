a2dbcfef7a5878f7650659630fc37a38ed3d5693
Frida Berg <fb223gw@student.lnu.se>
2025-09-29 18:10:46 +0200

    added summary metod

--- src/DocumentStats.js
+++ src/DocumentStats.js
@@ -8,4 +8,10 @@ export default class DocumentStats {
     getWordCount() {
         return this.wordAnalyzer.getWordCount()
     }
+
+     summary() {
+        return {
+            words: this.getWordCount()
+        }
+    }
   }
