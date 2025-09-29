97ec95b0078ac87b96bf8758fd0e2c6dede4b05d
Frida Berg <fb223gw@student.lnu.se>
2025-09-29 18:31:45 +0200

    Added Sentence funktion

--- src/DocumentStats.js
+++ src/DocumentStats.js
@@ -1,17 +1,24 @@
 import WordAnalyzer from './WordAnalyzer.js'
+import SentenceAnalyzer from './SentenceAnalyzer.js'
 
 export default class DocumentStats {
     constructor(text) {
         this.text = text;
         this.wordAnalyzer = new WordAnalyzer(text)
+        this.sentenceAnalyzer = new SentenceAnalyzer(text)
     }
     getWordCount() {
-        return this.wordAnalyzer.countWords()
+      return this.wordAnalyzer.countWords()
+    }
+
+    getSentenceCount() {
+      return this.sentenceAnalyzer.countSentences()
     }
 
      summary() {
         return {
-            words: this.getWordCount()
+            words: this.getWordCount(),
+            sentences: this.getSentenceCount()
         }
     }
   }
